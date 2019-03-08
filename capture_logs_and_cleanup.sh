dcb6b4e3738b780bf14e79fb16ddcfee36a0daae
Derrick Miller <derrick.f.miller@gmail.com>
2019-03-08 16:04:32 -0500

    simplified steps into a script to pause, capture logs, and remove stack

---
+++ capture_logs_and_cleanup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Pausing all curlloop running containers.."
+docker container pause $(docker container ps -q --filter "name=loop" --format "{{.Names}}")
+
+echo "Capturing all logs from paused curlloop containers..."
+for h in $(docker container ps -q --filter "name=curlloop" --format "{{.Names}}); 
+do 
+    echo "\tCapturings logs in docker-$h.logs"
+    docker container logs $h >> docker-$h.logs
+done
+
+echo "Removing Stack..."
+docker stack rm curlloop
+
+echo "All done. Go check out those .logs files for any not-200 HTTP status codes. Have a nice day!"
