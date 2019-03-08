89d34afd34575e9b23bfbed90fcc22e1177920f2
Derrick Miller <derrick.f.miller@gmail.com>
2019-03-08 16:07:48 -0500

    add friendly messages abot progress

--- save_images.sh
+++ save_images.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "Saving image curlloop:alpine-3.9 > curlloop:alpine-3.9.tar"
 docker save curlloop:alpine-3.9 > curlloop-alpine-3.9.tar
-docker save curlloop:centos-7> curlloop-centos-7.tar
+echo "Saving image curlloop:centos-7 > curlloop-centos-7.tar"
+docker save curlloop:centos-7 > curlloop-centos-7.tar
+echo "Saving image curlloop:ubuntu-18.04 > curlloop-ubuntu-18.04.tar"
 docker save curlloop:ubuntu-18.04 > curlloop-ubuntu-18.04.tar
