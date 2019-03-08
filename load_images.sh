a75bba4e76e91c2b5d0e8bb48f7ef5523beb6a19
Derrick Miller <derrick.f.miller@gmail.com>
2019-03-08 16:08:59 -0500

    add friendly messages about progress

--- load_images.sh
+++ load_images.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Loading curlloop-alpine-3.6.tar"
 docker load curlloop-alpine-3.6.tar
+echo "Loading curlloop-centos-7.tar"
 docker load curlloop-centos-7.tar
+echo "Loading curlloop-ubuntu-18.04.tar"
 docker load curlloop-ubuntu-18.04.tar
