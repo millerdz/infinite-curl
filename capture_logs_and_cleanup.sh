9940bcc9c148a5072fae195f3b3f82c96c52c461
Derrick Miller <derrick.f.miller@gmail.com>
2019-03-08 16:05:38 -0500

    fix typo

--- capture_logs_and_cleanup.sh
+++ capture_logs_and_cleanup.sh
@@ -4,7 +4,7 @@ echo "Pausing all curlloop running containers.."
 docker container pause $(docker container ps -q --filter "name=loop" --format "{{.Names}}")
 
 echo "Capturing all logs from paused curlloop containers..."
-for h in $(docker container ps -q --filter "name=curlloop" --format "{{.Names}}); 
+for h in $(docker container ps -q --filter "name=curlloop" --format "{{.Names}}"); 
 do 
     echo "\tCapturings logs in docker-$h.logs"
     docker container logs $h >> docker-$h.logs
